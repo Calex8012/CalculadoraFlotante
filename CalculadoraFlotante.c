224e039bb2a8e53493066050f10212afb3383ee7
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-05-15 22:59:27 -0500

    Variables

--- CalculadoraFlotante.c
+++ CalculadoraFlotante.c
@@ -1,11 +1,30 @@
 #include <CalculadoraFlotante.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int flag=0;
+float numero1=0,numero2=0;
+float resultado=0;
+float operacion=0;
+int puntero=0;
+float decimal=0,decimal2=0;
+void clean(void);
+int i=0;
+
+void clean(void){
+   numero1=0;
+   numero2=0;
+   operacion=0;
+   puntero=0;
+   for(int clear=0;clear<22;clear++){
+   
+}
+}
 void main()
 {
 
-	while(TRUE)
-	{
-		//TODO: User Code
-	}
-
+   while(TRUE)
+   {
+      //TODO: User Code
+   }
 }
