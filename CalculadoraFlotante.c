bdd1ed5b68a7db2b968b13c28c01678903c51b7d
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-07-02 22:36:03 -0500

    Calculadora Mejorada

--- CalculadoraFlotante.c
+++ CalculadoraFlotante.c
@@ -1,63 +1,53 @@
 #include <18F4620.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <Operacion.c>
+#include <Serial.c>
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
-#use delay(clock=16000000)
 
-#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
-
-#ifdef __DEBUG_SERIAL__
-   #define TX_232        PIN_C6
-   #define RX_232        PIN_C7
-   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1, UART1, RCV=RX_232)
-   #use fast_io(c)
-#endif
-
-int flagSerial=0,flagEcho=0,flagMostrar=0,flagEnter=0,PosicionFinal=0;
-int contador_buffer=0;
+int Posicion[3];
 char caracter;
 char buffer[30]={""};
 float num1,num2;
 //Variables de Conversión
 char opcion;
-char BufferNum1[10]={""};
-char BufferNum2[10]={""};
-
+#define TamanoVectorNumerico 9
+#define TamanoPosicionVector 3
 #INT_RDA
 void isrRDA(void){
    flagEcho=1;
    flagSerial=1;
    caracter=getc();
-   if(contador_buffer<=30){
-      buffer[contador_buffer]=caracter;
-      contador_buffer++;
+   if(ContadorBuffer<=30){
+      buffer[ContadorBuffer]=caracter;
+      ContadorBuffer++;
    }
+   if(caracter==0x0D){
+      flagEnter=1;
+   } 
 }
-   
    
 void main(void){
    set_tris_c(0x80);
    enable_interrupts(INT_RDA);
    enable_interrupts(GLOBAL);
-   while(1){
+   while(1){  
+      DeteccionPosiciones(flagEnter, Posicion, 3, buffer, ContadorBuffer);
+      AgregandoCaracteres(BufferNum1, TamanoVectorNumerico, BufferNum2, TamanoVectorNumerico, buffer, ContadorBuffer, Posicion, TamanoPosicionVector);
+      opcion=buffer[ContadorBuffer-2];
+      num1=atof(BufferNum1);
+      num2=atof(BufferNum2);
       if(flagSerial==1){
          putc(caracter);
          flagSerial=0;
       }
-      for(int i=0;i<31;i++)
-      {
-         if(buffer[i]==44)
-         {
-            PosicionFinal=i;
-         }
+      if(flagEnter==1){
+         printf("%.2f\n", operacion(opcion, num1, num2));
+         flagEnter=0;
       }
-      for(int j=0;j<=PosicionFinal;j++)
-      {
-         buffer[j]=BufferNum1[j];
-      }
-      
    }
-   
-  
 }
+
+
+
